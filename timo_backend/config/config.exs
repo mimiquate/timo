1d3dee442b7a5189120743db632e1e8fb2284203
Santiago Cabrera <santiago@mimiquate.com>
2020-01-23 11:42:13 -0300

    Added JSON API errors rendering in config

--- timo_backend/config/config.exs
+++ timo_backend/config/config.exs
@@ -14,7 +14,7 @@ config :timo,
 config :timo, TimoWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "RpRII5vT7qZm1Pk5hhaaZ8gx1x3twM1h4Zui2fVlapMp3be3TwPfy8LSdUZtNATU",
-  render_errors: [view: TimoWeb.ErrorView, accepts: ~w(json)],
+  render_errors: [view: TimoWeb.ErrorView, accepts: ~w(json json-api)],
   pubsub: [name: Timo.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
