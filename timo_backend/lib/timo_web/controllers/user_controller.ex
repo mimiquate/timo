3b58f011361c7e7f5084363e8a2f9e94ab266675
Santiago Cabrera <santiago@mimiquate.com>
2020-01-24 17:13:32 -0300

    Using cookies for session login

--- timo_backend/lib/timo_web/controllers/user_controller.ex
+++ timo_backend/lib/timo_web/controllers/user_controller.ex
@@ -10,20 +10,42 @@ defmodule TimoWeb.UserController do
   def create(conn, %{"data" => %{"type" => "users", "attributes" => user_params}}) do
     username = user_params["username"]
     with {:ok, %User{} = user} <- API.get_user_by_username(username) do
-      render(conn, "show.json-api", data: user)
+      conn
+      |> fetch_session()
+      |> put_session("user_id", user.id)
+      |> render("show.json-api", data: user)
     else
       nil -> create_render(conn, user_params)
     end
   end
 
+  def show(conn, %{"id" => "me"}) do
+    user_id =
+      conn
+      |> fetch_session()
+      |> get_session("user_id")
+
+    if !user_id do
+      {:error, :not_found}
+    else
+      user = API.get_user(user_id)
+      render(conn, "show.json-api", data: user)
+    end
+  end
+
   def show(conn, %{"id" => id}) do
-    user = API.get_user!(id)
-    render(conn, "show.json-api", data: user)
+    with %User{} = user <- API.get_user(id) do
+      render(conn, "show.json-api", data: user)
+    else
+      _ -> {:error, :not_found}
+    end
   end
 
   defp create_render(conn, user_params) do
     with {:ok, %User{} = user} <- API.create_user(user_params) do
       conn
+      |> fetch_session()
+      |> put_session("user_id", user.id)
       |> put_status(:created)
       |> put_resp_header("location", Routes.user_path(conn, :show, user))
       |> render("show.json-api", data: user)
