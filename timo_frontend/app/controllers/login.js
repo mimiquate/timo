c4a5d0f8a53e0c863dd01c3b1b7e098598626878
Santiago Cabrera <santiago@mimiquate.com>
2020-01-27 17:21:03 -0300

    Ember paper installation and implementation.

--- timo_frontend/app/controllers/login.js
+++ timo_frontend/app/controllers/login.js
@@ -2,31 +2,25 @@ import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  showErrors: false,
-
   router: service(),
   session: service(),
 
   actions: {
-    async getIn(event) {
-      event.preventDefault();
-  
+    async getIn() {
       let { username } = this;
-      if (!username) {
-        this.send('setError');
-      } else {
-        let user = this.store.createRecord('user', { username });
-        await user.save()
+      let newUsername = username.trim();
+
+      this.set("username", newUsername);
+
+      if (newUsername) {
+        let user = this.store.createRecord('user', { newUsername });
+        await user.save();
         await this.router.transitionTo('landing');
       }
     },
-  
-    setError() {
-      this.set('showErrors', true)
-    },
-  
-    resetError() {
-      this.set('showErrors', false);
+
+    setValue(value) {
+      this.set('username', value);
     }
   }
 });
