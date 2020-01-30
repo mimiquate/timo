f979c7220902917a2632ccbfbbc27a750e4b4275
Santiago Cabrera <santiago@mimiquate.com>
2020-01-30 16:16:33 -0300

    Modify API and team_controller

--- timo_backend/lib/timo_web/controllers/team_controller.ex
+++ timo_backend/lib/timo_web/controllers/team_controller.ex
@@ -3,41 +3,34 @@ defmodule TimoWeb.TeamController do
 
   alias Timo.API
   alias Timo.API.Team
-  alias JaSerializer.Params
 
   action_fallback TimoWeb.FallbackController
 
-  def index(conn, _params) do
-    teams = API.list_teams()
+  def index(conn, _params, current_user) do
+    teams = API.list_user_teams(current_user)
     render(conn, "index.json-api", data: teams)
   end
 
-  def create(conn, %{"data" => data = %{"type" => "team", "attributes" => team_params}}) do
-    with {:ok, %Team{} = team} <- API.create_team(team_params) do
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", Routes.team_path(conn, :show, team))
-      |> render("show.json-api", data: team)
-    end
+  def create(conn, %{"data" => %{"type" => "team", "attributes" => team_params}},
+    current_user) do
+      with {:ok, %Team{} = team} <- API.create_team(current_user, team_params) do
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", Routes.team_path(conn, :show, team))
+        |> render("show.json-api", data: team)
+      end
   end
 
-  def show(conn, %{"id" => id}) do
-    team = API.get_team!(id)
-    render(conn, "show.json-api", data: team)
-  end
-
-  def update(conn, %{"id" => id, "data" => data = %{"type" => "team", "attributes" => team_params}}) do
-    team = API.get_team!(id)
-
-    with {:ok, %Team{} = team} <- API.update_team(team, team_params) do
+  def show(conn, %{"id" => id}, current_user) do
+    with {:ok, %Team{} = team} <- API.get_user_team(current_user, id) do
       render(conn, "show.json-api", data: team)
+    else
+      _ -> {:error, :not_found}
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    team = API.get_team!(id)
-    with {:ok, %Team{}} <- API.delete_team(team) do
-      send_resp(conn, :no_content, "")
-    end
+  def action(conn, _) do
+    args = [conn, conn.params, conn.assigns.current_user]
+    apply(__MODULE__, action_name(conn), args)
   end
 end
--- timo_backend/test/timo_web/controllers/team_controller_test.exs
+++ timo_backend/test/timo_web/controllers/team_controller_test.exs
@@ -2,10 +2,10 @@ defmodule TimoWeb.TeamControllerTest do
   use TimoWeb.ConnCase
 
   alias Timo.API
+  alias Timo.API.User
   alias Timo.API.Team
 
   @create_attrs %{name: "some name"}
-  @update_attrs %{name: "some updated name"}
   @invalid_attrs %{name: nil}
 
   def fixture(:team) do
@@ -13,93 +13,101 @@ defmodule TimoWeb.TeamControllerTest do
     team
   end
 
+  def user_fixture(attrs \\ %{}) do
+    {:ok, user} =
+      attrs
+      |> Enum.into(%{username: "some username"})
+      |> API.create_user()
+
+    user
+  end
+
+  def team_fixture(%User{} = user, attrs \\ %{}) do
+    attrs = Enum.into(attrs, %{name: "some name"})
+    {:ok, team} = API.create_team(user, attrs)
+
+    team
+  end
+
+  def data_fixture(attribute) do
+    %{
+      "meta" => %{},
+      "data" => %{
+        "type" => "team",
+        "attributes" => attribute,
+        "relationships" => relationships()
+      }
+    }
+  end
   
-  defp relationships do
+  defp relationships() do
     %{}
   end
 
   setup %{conn: conn} do
+    user = user_fixture()
     conn = conn
+      |> assign(:current_user, user)
       |> put_req_header("accept", "application/vnd.api+json")
       |> put_req_header("content-type", "application/vnd.api+json")
       
-    {:ok, conn: conn}
+    {:ok, conn: conn, user: user}
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, team_path(conn, :index)
+  test "lists all entries on index", %{conn: conn, user: user} do
+    team1 = team_fixture(user)
+    team2 = team_fixture(user)
+    conn = get(conn, Routes.team_path(conn, :index))
+
+    [data1, data2 | []] = json_response(conn, 200)["data"]
+
+    assert data1["type"] == "team"
+    assert data2["type"] == "team"
+    assert Integer.to_string(team1.id) == data1["id"]
+    assert Integer.to_string(team2.id) == data2["id"]
+
+    assert API.list_user_teams(user) == [team1, team2]
+  end
+
+  test "lists empty entries on index", %{conn: conn} do
+    conn = get(conn, Routes.team_path(conn, :index))
+
     assert json_response(conn, 200)["data"] == []
   end
 
-  test "creates team and renders team when data is valid", %{conn: conn} do
-    conn = post conn, team_path(conn, :create), %{
-      "meta" => %{},
-      "data" => %{
-        "type" => "team",
-        "attributes" => @create_attrs,
-        "relationships" => relationships
-      }
-    }
-    assert %{"id" => id} = json_response(conn, 201)["data"]
+  test "creates team and renders team when data is valid", %{conn: conn, user: user} do
+    conn = post(conn, Routes.team_path(conn, :create), data_fixture(@create_attrs))
 
-    conn = get conn, team_path(conn, :show, id)
+    assert data = json_response(conn, 201)["data"]
+    assert data["type"] == "team"
+    assert data["attributes"]["name"] == @create_attrs.name
+
+    {:ok, %Team{} = team} = API.get_user_team(user, data["id"])
+
+    assert Integer.to_string(team.id) == data["id"]
+    assert team.user_id == user.id
+    assert team.name == data["attributes"]["name"]
+  end
+
+  test "does not create team and renders errors when data is invalid", %{conn: conn} do
+    conn = post(conn, Routes.team_path(conn, :create), data_fixture(@invalid_attrs))
+    assert json_response(conn, 422)["errors"] != %{}
+  end
+
+  test "show video of user", %{conn: conn, user: user} do
+    team = team_fixture(user)
+    team_id = Integer.to_string(team.id)
+    conn = get(conn, Routes.team_path(conn, :show, team_id))
     data = json_response(conn, 200)["data"]
-    assert data["id"] == id
+
+    assert data["id"] == team_id
     assert data["type"] == "team"
     assert data["attributes"]["name"] == @create_attrs.name
   end
 
-  test "does not create team and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, team_path(conn, :create), %{
-      "meta" => %{},
-      "data" => %{
-        "type" => "team",
-        "attributes" => @invalid_attrs,
-        "relationships" => relationships
-      }
-    }
-    assert json_response(conn, 422)["errors"] != %{}
-  end
+  test "does not show video that doesn't exist", %{conn: conn} do
+    conn = get(conn, Routes.team_path(conn, :show, 100))
 
-  test "updates chosen team and renders team when data is valid", %{conn: conn} do
-    %Team{id: id} = team = fixture(:team)
-    conn = put conn, team_path(conn, :update, team), %{
-      "meta" => %{},
-      "data" => %{
-        "type" => "team",
-        "id" => "#{team.id}",
-        "attributes" => @update_attrs,
-        "relationships" => relationships
-      }
-    }
-
-    conn = get conn, team_path(conn, :show, id)
-    data = json_response(conn, 200)["data"]
-    assert data["id"] == "#{id}"
-    assert data["type"] == "team"
-    assert data["attributes"]["name"] == @update_attrs.name
-  end
-
-  test "does not update chosen team and renders errors when data is invalid", %{conn: conn} do
-    team = fixture(:team)
-    conn = put conn, team_path(conn, :update, team), %{
-      "meta" => %{},
-      "data" => %{
-        "type" => "team",
-        "id" => "#{team.id}",
-        "attributes" => @invalid_attrs,
-        "relationships" => relationships
-      }
-    }
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "deletes chosen team", %{conn: conn} do
-    team = fixture(:team)
-    conn = delete conn, team_path(conn, :delete, team)
-    assert response(conn, 204)
-    assert_error_sent 404, fn ->
-      get conn, team_path(conn, :show, team)
-    end
+    assert json_response(conn, 404)["errors"] != %{}
   end
 end
