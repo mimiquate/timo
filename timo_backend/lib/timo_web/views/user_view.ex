62a4d6b2060b377c59c71cb427ffb8bfb32f7c15
Santiago Cabrera <santiago@mimiquate.com>
2020-02-06 09:47:44 -0300

    Delete useless user hasmany serializer

--- timo_backend/lib/timo_web/views/user_view.ex
+++ timo_backend/lib/timo_web/views/user_view.ex
@@ -3,9 +3,4 @@ defmodule TimoWeb.UserView do
   use JaSerializer.PhoenixView
 
   attributes([:username, :inserted_at, :updated_at])
-
-  has_many :teams,
-    serializer: TimoWeb.TeamView,
-    include: false,
-    identifiers: :when_included
 end
