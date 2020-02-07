9d276a6c057cf776d68c8124acbcaf5f85f93058
Santiago Cabrera <santiago@mimiquate.com>
2020-02-07 17:54:00 -0300

    Fix member view

--- timo_backend/lib/timo_web/views/member_view.ex
+++ timo_backend/lib/timo_web/views/member_view.ex
@@ -2,5 +2,5 @@ defmodule TimoWeb.MemberView do
   use TimoWeb, :view
   use JaSerializer.PhoenixView
 
-  attributes([:name, :timezone, :inserted_at, :updated_at])
+  attributes([:name, :timezone])
 end
