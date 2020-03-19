e076597341b3805a85e4cd1ccf41f1508b246edb
Santiago Cabrera <santiago@mimiquate.com>
2020-03-19 11:45:25 -0300

    Delete comments

--- timo_backend/lib/timo_web/controllers/session_controller.ex
+++ timo_backend/lib/timo_web/controllers/session_controller.ex
@@ -7,17 +7,6 @@ defmodule TimoWeb.SessionController do
     conn
     |> fetch_session()
     |> delete_session("user_id")
-    # ---- Pass mix test, resp_body="\"\"", ember parsing error
-    # |> put_status(:no_content)
-    # |> json("")
-    # ---- Doesn't pass mix test
-    # |> put_resp_header("content-type", "application/vnd.api+json")
-    # |> resp(:no_content, '')
-    # |> send_resp()
-    # ---- What json function actually does, uses api+json in this one, ember parsing error
-    # |> put_resp_header("content-type", "application/vnd.api+json")
-    # |> send_resp(:no_content, response)
-    # ---- Functions correctly but uses response 200 instead of 204
     |> put_resp_header("content-type", "application/vnd.api+json")
     |> resp(:ok, response)
     |> send_resp()
