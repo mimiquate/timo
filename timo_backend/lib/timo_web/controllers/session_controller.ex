e4228f4858ff5cabcdfbb4a7121fb2b69f0b4ee0
Santiago Cabrera <santiago@mimiquate.com>
2020-03-19 17:31:33 -0300

    Use json parser for session response

--- timo_backend/lib/timo_web/controllers/session_controller.ex
+++ timo_backend/lib/timo_web/controllers/session_controller.ex
@@ -2,12 +2,8 @@ defmodule TimoWeb.SessionController do
   use TimoWeb, :controller
 
   def logout(conn, _params) do
-    response = Phoenix.json_library().encode_to_iodata!(%{})
-
     conn
     |> delete_session("user_id")
-    |> put_resp_header("content-type", "application/vnd.api+json")
-    |> resp(:ok, response)
-    |> send_resp()
+    |> json(%{})
   end
 end
