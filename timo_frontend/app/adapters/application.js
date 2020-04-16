249fd7813b8349afaf8a4633e5abe0d85b27b5fa
Santiago Cabrera <santiago@mimiquate.com>
2020-04-16 12:40:20 -0300

    Delete useless DataAdapterMixin in application adapter

--- timo_frontend/app/adapters/application.js
+++ timo_frontend/app/adapters/application.js
@@ -1,6 +1,5 @@
 import DS from 'ember-data';
-import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
+export default DS.JSONAPIAdapter.extend({
   namespace: 'api'
 });
