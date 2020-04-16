ae202e434fd6dd56f19ae958b69c91eb109581d5
Santiago Cabrera <santiago@mimiquate.com>
2020-04-16 12:16:39 -0300

    Delete header in adapter when not using token for auth

--- timo_frontend/app/adapters/application.js
+++ timo_frontend/app/adapters/application.js
@@ -1,16 +1,6 @@
 import DS from 'ember-data';
-import { computed } from '@ember/object';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  namespace: 'api',
-
-  headers: computed('session.data.authenticated.access_token', function() {
-    let headers = {};
-    if (this.session.isAuthenticated) {
-      headers['Authorization'] = `Bearer ${this.session.data.authenticated.token}`;
-    }
-
-    return headers;
-  })
+  namespace: 'api'
 });
