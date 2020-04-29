45f3101f133a3d1c67fc7b3c17a64de22f58c58c
Santiago Cabrera <santiago@mimiquate.com>
2020-04-29 13:38:41 -0300

    Frontend verification

---
+++ timo_frontend/app/routes/verify/token.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  async model(params) {
+    const adapter = await this.store.adapterFor('user');
+
+    return adapter.verifyEmail(params.token)
+      .then(() => this.currentUser.load());
+  }
+});
