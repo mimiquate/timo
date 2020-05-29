690bb5a0fe4ea85056385b4f1ca3c8a208edeee9
Marcelo Dominguez <marcelo@mimiquate.com>
2020-05-29 13:33:23 -0300

    Update backend url

--- timo_frontend/config/environment.js
+++ timo_frontend/config/environment.js
@@ -55,7 +55,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // here you can enable a production-specific feature
-    ENV.serverHost = 'https://blooming-dawn-41319.herokuapp.com';
+    ENV.serverHost = 'https://timo-backend.herokuapp.com';
   }
 
   return ENV;
