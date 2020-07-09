3bcab1dfda0a8b8af12a103e5ff5204211cd570d
Santiago Cabrera <santiago@mimiquate.com>
2020-07-09 11:49:06 -0300

    Create computed for public checkbox

--- timo_frontend/app/components/table-options-menu.js
+++ timo_frontend/app/components/table-options-menu.js
@@ -1,8 +1,14 @@
 import Component from '@ember/component';
 import copyTextToClipboard from 'timo-frontend/utils/copy-text-to-clipboard';
 import { set } from "@ember/object";
+import { computed } from "@ember/object";
+import { isEmpty } from '@ember/utils';
 
 export default Component.extend({
+  disablePublic: computed('team.{public,share_id}', function () {
+    return !this.team.public || isEmpty(this.team.share_id)
+  }),
+
   actions: {
     copyLink() {
       const { protocol, host } = window.location;
