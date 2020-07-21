77c3238bed070a547e196952599c11aa2829449e
Santiago Cabrera <santiago@mimiquate.com>
2020-07-21 15:10:59 -0300

    Run ember-cli-update to version 3.16.0

--- timo_frontend/app/router.js
+++ timo_frontend/app/router.js
@@ -1,10 +1,10 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
+export default class Router extends EmberRouter {
+  location = config.locationType;
+  rootURL = config.rootURL;
+}
 
 Router.map(function () {
   this.route('landing', { path: '' }, function () {
@@ -25,5 +25,3 @@ Router.map(function () {
     this.route('token', { path: ':token' });
   });
 });
-
-export default Router;
