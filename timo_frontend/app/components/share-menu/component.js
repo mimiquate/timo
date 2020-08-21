2a087d3d2aecefc24e861738347eb5c6bd8f4d4f
Kevin Exposito <kevin@mimiquate.com>
2020-08-21 14:24:26 -0300

    Remove PaperMenu from Share button

--- timo_frontend/app/components/share-menu/component.js
+++ timo_frontend/app/components/share-menu/component.js
@@ -2,11 +2,19 @@ import Component from '@glimmer/component';
 import copyTextToClipboard from 'timo-frontend/utils/copy-text-to-clipboard';
 import { computed, action } from '@ember/object';
 import { isEmpty } from '@ember/utils';
+import { tracked } from '@glimmer/tracking';
 
 export default class ShareMenuComponent extends Component {
+  @tracked showToggleablePopover = false;
+
   @computed('args.team.{public,share_id}')
   get disablePublic() {
     return !this.args.team.public || isEmpty(this.args.team.share_id);
+  }
+
+  @action
+  togglePopover() {
+    this.showToggleablePopover = !this.showToggleablePopover;
   }
 
   @action
@@ -19,8 +27,8 @@ export default class ShareMenuComponent extends Component {
   }
 
   @action
-  async setPublic(value) {
-    this.args.team.public = value;
+  async setPublic() {
+    this.args.team.public = !this.args.team.public;
     await this.args.team.save();
   }
 }
