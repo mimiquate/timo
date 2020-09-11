58229f8ea6cec7efbd7dec1e54c928e1f34b2c96
Kevin Exposito <kevin@mimiquate.com>
2020-09-11 13:54:56 -0300

    Add clean error to sign-up and login inputs

--- timo_frontend/app/components/t-input/component.js
+++ timo_frontend/app/components/t-input/component.js
@@ -2,8 +2,10 @@ import Component from "@glimmer/component";
 import { action } from '@ember/object';
 
 export default class TInputComponent extends Component {
+
   @action
   onChange(e) {
+    this.args.cleanError();
     this.args.onChange(e.target.value);
   }
 }
