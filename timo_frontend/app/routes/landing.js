0cb35e4a8e6efcb19b59440e653db36f89edce37
Kevin Exposito <kevin@mimiquate.com>
2020-09-22 13:41:03 -0300

    Fix test

--- timo_frontend/app/routes/landing.js
+++ timo_frontend/app/routes/landing.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default class LandingRoute extends Route.extend(AuthenticatedRouteMixin) {
-  afterModel(model, transition) {
+  afterModel(model) {
     if (model.length !== 0) {
       const teamToTransition = model.toArray().map(t => parseInt(t.id));
       const id = Math.min(...teamToTransition);
