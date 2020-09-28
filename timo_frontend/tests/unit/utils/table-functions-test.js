6d9342fc7c23e5205df5f40b8cc1c19e54226ad7
Santiago Cabrera <santiago@mimiquate.com>
2020-09-28 16:20:00 -0300

    Test split timezone

--- timo_frontend/tests/unit/utils/table-functions-test.js
+++ timo_frontend/tests/unit/utils/table-functions-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { compareMemberTimeZones } from 'timo-frontend/utils/table-functions'
+import { compareMemberTimeZones, splitTimezone } from 'timo-frontend/utils/table-functions'
 
 module('Unit | Utils | table functions', function (hooks) {
   setupTest(hooks);
@@ -13,4 +13,23 @@ module('Unit | Utils | table functions', function (hooks) {
     assert.equal(compareMemberTimeZones(memberSmallerTZ, memberBiggerTZ), -1);
     assert.equal(compareMemberTimeZones(memberBiggerTZ, memberBiggerTZ), 0);
   });
+
+  test('Split current timezone', function (assert) {
+    const timezone = "America/Montevideo";
+    const expected = "America, Montevideo (you)"
+
+    const timezoneSplited = splitTimezone(timezone, timezone);
+
+    assert.equal(timezoneSplited, expected, "Correct text");
+  })
+
+  test('Split complex timezone', function (assert) {
+    const timezoneNow = "America/Montevideo";
+    const timezone = "America/Argentina/Buenos_Aires"
+    const expected = "America, Argentina, Buenos Aires"
+
+    const timezoneSplited = splitTimezone(timezone, timezoneNow);
+
+    assert.equal(timezoneSplited, expected, "Correct text");
+  })
 });
