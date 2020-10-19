6c298483c670fcd9e4c5aa3a074846a3650e0378
Kevin Exposito <kevin@mimiquate.com>
2020-10-19 13:45:53 -0300

    Add header tooltip

--- timo_frontend/app/components/team-header/component.js
+++ timo_frontend/app/components/team-header/component.js
@@ -1,9 +1,24 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
 
 export default class TeamHeaderComponent extends Component {
+  @service media;
+  @tracked showTeamOptions = false;
+
   get membersLabel() {
     const members = this.args.members.length;
 
     return members > 1 ? `${members} Members` : `${members} Member`
+  }
+
+  @action
+  openTeamOptions() {
+    this.showTeamOptions = true;
+  }
+
+  closeTeamOptions() {
+    this.showTeamOptions = false;
   }
 }
