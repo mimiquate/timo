418fda521b52173237b6014539e97d485bc13fc2
Kevin Exposito <kevin@mimiquate.com>
2020-11-25 10:58:35 -0300

    Use ember pluralize for members

--- timo_frontend/app/components/shared-team-header/component.js
+++ timo_frontend/app/components/shared-team-header/component.js
@@ -1,19 +1,12 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { pluralize } from 'ember-inflector';
 
 export default class SharedTeamHeaderComponent extends Component {
   @service session;
   @service media;
 
-  get membersLabel() {
-    const members = this.args.members.length - 1;
-
-    if (members === 0) {
-      return 'No members'
-    } else if (members === 1) {
-      return `${members} Member`;
-    } else {
-      return `${members} Members`
-    }
+  get membersLength() {
+    return this.args.members.length - 1;
   }
 }
