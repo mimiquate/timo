b5dc1c0f5ed32c064357140ad86736f17af01bb5
Kevin Exposito <kevin@mimiquate.com>
2020-12-01 12:23:07 -0300

    Remove sentry in development

--- timo_frontend/config/environment.js
+++ timo_frontend/config/environment.js
@@ -50,7 +50,8 @@ module.exports = function(environment) {
       // of transactions for performance monitoring.
       // We recommend adjusting this value in production, or using tracesSampler
       // for finer control
-      tracesSampleRate: 1.0
+      tracesSampleRate: 1.0,
+      development: false
     }
   };
 
