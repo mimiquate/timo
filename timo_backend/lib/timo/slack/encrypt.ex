b0d8499265230b4a0f5990253779548c5b2d3f2d
Kevin Exposito <kevin@mimiquate.com>
2020-12-21 09:41:06 -0300

    Rename decoded key

--- timo_backend/lib/timo/slack/encrypt.ex
+++ timo_backend/lib/timo/slack/encrypt.ex
@@ -6,7 +6,7 @@ defmodule Timo.Slack.Encrypt do
     iv = :crypto.strong_rand_bytes(16)
 
     {ciphertext, ciphertag} =
-      :crypto.block_encrypt(:aes_gcm, decode_secret_key(), iv, {@aad, to_string(val), 16})
+      :crypto.block_encrypt(:aes_gcm, key(), iv, {@aad, to_string(val), 16})
 
     (iv <> ciphertag <> ciphertext)
     |> :base64.encode()
@@ -17,10 +17,10 @@ defmodule Timo.Slack.Encrypt do
 
     <<iv::binary-16, tag::binary-16, ciphertext::binary>> = ciphertext
 
-    :crypto.block_decrypt(:aes_gcm, decode_secret_key(), iv, {@aad, ciphertext, tag})
+    :crypto.block_decrypt(:aes_gcm, key(), iv, {@aad, ciphertext, tag})
   end
 
-  defp decode_secret_key() do
+  defp key() do
     :base64.decode(@encrypt_secret_key)
   end
 end
