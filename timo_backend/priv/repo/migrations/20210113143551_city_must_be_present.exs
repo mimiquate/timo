ac4f3d53a37d5760668283eb4cc15aa4aa01b824
Kevin Exposito <kevin@mimiquate.com>
2021-01-13 15:09:23 -0300

    Migration to avoid empty cities

---
+++ timo_backend/priv/repo/migrations/20210113143551_city_must_be_present.exs
@@ -0,0 +1,19 @@
+defmodule Timo.Repo.Migrations.CityMustBePresent do
+  use Ecto.Migration
+
+  def up do
+    drop (constraint("members", "members_city_id_fkey"))
+
+    alter table("members") do
+      modify :city_id, references(:cities, on_delete: :nilify_all, on_update: :update_all), null: false
+    end
+  end
+
+  def down do
+    drop (constraint("members", "members_city_id_fkey"))
+
+    alter table("members") do
+      modify :city_id, references(:cities, on_delete: :nilify_all, on_update: :update_all)
+    end
+  end
+end
