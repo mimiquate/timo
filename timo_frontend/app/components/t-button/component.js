c097f7f545424766d825f0f8227c6511d71cc7c1
Kevin Exposito <kevin@mimiquate.com>
2021-11-15 14:47:41 -0300

    Fix fire form action twice on login and sign up

--- timo_frontend/app/components/t-button/component.js
+++ timo_frontend/app/components/t-button/component.js
@@ -1,7 +1,16 @@
 import Component from "@glimmer/component";
+import { action, get } from '@ember/object';
+import { isPresent } from '@ember/utils';
 
 export default class TButtonComponent extends Component {
   get buttonType() {
     return this.args.buttonType || "button";
+  }
+
+  @action
+  onClick() {
+    if (isPresent(get(this.args, 'onClick'))) {
+      this.args.onClick()
+    }
   }
 }
