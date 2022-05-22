0c79756d5c217bbeba43c0e0fd17f316c0358681
Marcelo Dominguez <marcelo@mimiquate.com>
2022-05-22 15:42:51 -0300

    mix format

--- timo_backend/config/runtime.exs
+++ timo_backend/config/runtime.exs
@@ -24,16 +24,16 @@ if config_env() == :prod do
   database_url =
     System.get_env("DATABASE_URL") ||
       raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-  """
+          environment variable DATABASE_URL is missing.
+          For example: ecto://USER:PASS@HOST/DATABASE
+      """
 
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :timo, Timo.Repo,
-  url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-  socket_options: maybe_ipv6
+    url: database_url,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+    socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
@@ -43,26 +43,26 @@ if config_env() == :prod do
   secret_key_base =
     System.get_env("SECRET_KEY_BASE") ||
       raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-  """
+          environment variable SECRET_KEY_BASE is missing.
+          You can generate one by calling: mix phx.gen.secret
+      """
 
   host = System.get_env("PHX_HOST") || "timo-backend.mimiquate.xyz"
   port = String.to_integer(System.get_env("PORT") || "4000")
   cookie_signing_salt = System.get_env("COOKIE_SIGNING_SALT") || "abc"
 
-   config :timo, TimoWeb.Endpoint,
-   url: [host: host, port: 443, scheme: "https"],
-   http: [
-       # Enable IPv6 and bind on all interfaces.
-       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-       # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-       # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-     ip: {0, 0, 0, 0, 0, 0, 0, 0},
-     port: port
-   ],
-   secret_key_base: secret_key_base,
-   cookie_signing_salt: cookie_signing_salt
+  config :timo, TimoWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0},
+      port: port
+    ],
+    secret_key_base: secret_key_base,
+    cookie_signing_salt: cookie_signing_salt
 
   config :timo, frontend_url: System.get_env("FRONTEND_URL")
 
